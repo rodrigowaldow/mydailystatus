46969d5a27ddb77b4ccab9014be314ecca9d16cd
Rodrigo Waldow <waldowrodrigo@gmial.com>
2020-04-13 16:24:31 -0300

    [UPD] from status

--- pages/create-status.js
+++ pages/create-status.js
@@ -52,7 +52,7 @@ export default function CreateStatus() {
         <form className="bg-white shadow-md rounded pt-6 pb-8 mb-4">
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" for="febre">
-              Possui febre?
+              Você teve febre?
             </label>
             <label className="block">
               <input className="" type="radio" name="febre" value="Sim" />Sim
@@ -63,104 +63,56 @@ export default function CreateStatus() {
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="febre">
-              Está com dor de cabeça?
+            <label className="block text-gray-700 text-sm font-bold mb-2" for="sintomas">
+              Você teve algum desses sintomas?
             </label>
             <label className="block">
-              <input className="" type="radio" name="febre" value="Sim" />Sim
+              <input className="" type="checkbox" name="sintomas" />Coriza
             </label>
             <label className="block">
-              <input className="" type="radio" name="febre" value="Não" defaultChecked />Não
+              <input className="" type="checkbox" name="sintomas" />Nariz entupido
+            </label>
+            <label className="block">
+              <input className="" type="checkbox" name="sintomas" />Cansaço
+            </label>
+            <label className="block">
+              <input className="" type="checkbox" name="sintomas" />Tosse
+            </label>
+            <label className="block">
+              <input className="" type="checkbox" name="sintomas" />Dor de cabeça
+            </label>
+            <label className="block">
+              <input className="" type="checkbox" name="sintomas" />Dores no corpo ou mal estar
+            </label>
+            <label className="block">
+              <input className="" type="checkbox" name="sintomas" />Dor de garganta
+            </label>
+            <label className="block">
+              <input className="" type="checkbox" name="sintomas" />Diarréia ou dores abdominais
+            </label>
+            <label className="block">
+              <input className="" type="checkbox" name="sintomas" />Perda de olfato (não sentir cheiro)
+            </label>
+            <label className="block">
+              <input className="" type="checkbox" name="sintomas" />Perda do paladar (não sentir gosto)
             </label>
           </div>
 
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" for="febre">
-              Sente dores no corpo?
+              Você teve algum desses outros sintomas?
             </label>
             <label className="block">
-              <input className="" type="radio" name="febre" value="Sim" />Sim
+              <input className="" type="checkbox" name="febre" value="Sim" />Tive vomitos ou convulsão ou falta de ar ou dificuldade para respirar ou dedos azulados e pálidos
             </label>
             <label className="block">
-              <input className="" type="radio" name="febre" value="Não" defaultChecked />Não
+              <input className="" type="checkbox" name="febre" value="Não" defaultChecked />Não tive nenhum outro sintoma
             </label>
           </div>
 
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" for="febre">
-              Tem cansaço, fadiga?
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Sim" />Sim
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Não" defaultChecked />Não
-            </label>
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="febre">
-              Sentimento de exautão extrema?
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Sim" />Sim
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Não" defaultChecked />Não
-            </label>
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="febre">
-              Possui congestão nasal?
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Sim" />Sim
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Não" defaultChecked />Não
-            </label>
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="febre">
-              Espirra com frequência?
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Sim" />Sim
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Não" defaultChecked />Não
-            </label>
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="febre">
-              Tem dor de garganta?
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Sim" />Sim
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Não" defaultChecked />Não
-            </label>
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="febre">
-              Tem tosse seca?
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Sim" />Sim
-            </label>
-            <label className="block">
-              <input className="" type="radio" name="febre" value="Não" defaultChecked />Não
-            </label>
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="febre">
-              Sente falta de ar?
+              Você está grávida ou tem mais de 80 anos?
             </label>
             <label className="block">
               <input className="" type="radio" name="febre" value="Sim" />Sim
@@ -171,8 +123,8 @@ export default function CreateStatus() {
           </div>
         </form>
       </div>
-      <button onClick={getMyLocation}>Buscar minha localização</button>
-      <button onClick={save}>Salvar meu status</button>
+      <button onClick={getMyLocation} className="my-8 py-4 px-2 rounded bg-pink-800 font-bold shadow-xl hover:shadow block w-1/4 text-center mx-auto text-white">Buscar minha localização</button>
+      <button onClick={save} className="my-8 py-4 px-2 rounded bg-pink-800 font-bold shadow-xl hover:shadow block w-1/4 text-center mx-auto text-white">Salvar meu status</button>
     </div>
   );
 }
